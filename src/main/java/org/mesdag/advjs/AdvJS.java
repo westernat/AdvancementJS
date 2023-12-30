b5f495d80712715166e39657e6fa9bd02f440b58
Westernat <westernat233@163.com>
2023-12-30 14:08:08 +0800

    支持热重载

--- src/main/java/org/mesdag/advjs/AdvJS.java
+++ src/main/java/org/mesdag/advjs/AdvJS.java
@@ -1,12 +1,7 @@
 package org.mesdag.advjs;
 
 import com.mojang.logging.LogUtils;
-import dev.latvian.mods.kubejs.script.ScriptType;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 
@@ -16,13 +11,5 @@ public class AdvJS {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public AdvJS() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bus.addListener(this::commonSetup);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-        new AdvCreateEvent().post(ScriptType.STARTUP, "advjs");
-        LOGGER.info("AdvJS Loaded!");
     }
 }
