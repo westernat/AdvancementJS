e0ebae56425fa963b2bd42bcd4d2e0d2342b32d2
Westernat <westernat233@163.com>
2024-01-02 13:09:13 +0800

    StatePredicate支持"state1=true,state2=0"的格式

--- src/main/java/org/mesdag/advjs/predicate/StatePropertiesPredicateBuilder.java
+++ src/main/java/org/mesdag/advjs/predicate/StatePropertiesPredicateBuilder.java
@@ -7,9 +7,20 @@ import net.minecraft.world.level.block.state.properties.Property;
 class StatePropertiesPredicateBuilder {
     final StatePropertiesPredicate.Builder builder = StatePropertiesPredicate.Builder.properties();
 
-    public StatePropertiesPredicateBuilder matchProperty(String key, String value) {
+    public StatePropertiesPredicateBuilder match(String key, String value) {
         Property<Boolean> property = BooleanProperty.create(key);
         builder.hasProperty(property, value);
+        return this;
+    }
+
+    public StatePropertiesPredicateBuilder matchAll(String pairsString) {
+        String[] pairStrings = pairsString.split(",");
+        for (String pairString : pairStrings) {
+            String[] pair = pairString.split("=");
+            if (pair.length == 2) {
+                return match(pair[0].strip(), pair[1].strip());
+            }
+        }
         return this;
     }
 }
