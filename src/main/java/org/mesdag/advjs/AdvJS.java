fd58c64ad05bc1d4a0dcf892de32df23a31fcdba
Westernat <westernat233@163.com>
2024-01-05 15:32:05 +0800

    新增配置

--- src/main/java/org/mesdag/advjs/AdvJS.java
+++ src/main/java/org/mesdag/advjs/AdvJS.java
@@ -1,17 +1,23 @@
 package org.mesdag.advjs;
 
+import dev.latvian.mods.kubejs.CommonProperties;
+import dev.latvian.mods.kubejs.KubeJSPaths;
 import dev.latvian.mods.kubejs.bindings.event.ServerEvents;
 import dev.latvian.mods.kubejs.event.EventHandler;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+
 
 @Mod("advjs")
 public class AdvJS {
     public static final Logger LOGGER = LoggerFactory.getLogger("AdvJS");
     public static final EventHandler ADVANCEMENT = ServerEvents.GROUP.server("advancement", () -> AdvCreateEvent.class);
+    public static final Path EXAMPLE = KubeJSPaths.SERVER_SCRIPTS.resolve("advancement.js");
 
     public AdvJS() {
+        CommonProperties.reload();
     }
 }
--- src/main/java/org/mesdag/advjs/AdvJSPlugin.java
+++ src/main/java/org/mesdag/advjs/AdvJSPlugin.java
@@ -1,5 +1,6 @@
 package org.mesdag.advjs;
 
+import dev.latvian.mods.kubejs.CommonProperties;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
 import net.minecraft.advancements.FrameType;
@@ -10,7 +11,11 @@ import org.mesdag.advjs.util.GameTypeWrapper;
 import org.mesdag.advjs.util.RequirementsStrategyWrapper;
 import org.mesdag.advjs.util.StatsWrapper;
 
+import java.nio.file.Files;
+
 public class AdvJSPlugin extends KubeJSPlugin {
+    public static boolean DEBUG;
+
     @Override
     public void registerBindings(BindingsEvent event) {
         event.add("FrameType", FrameTypeWrapper.class);
@@ -29,5 +34,84 @@ public class AdvJSPlugin extends KubeJSPlugin {
         event.add("CREATIVE", GameType.CREATIVE);
         event.add("ADVENTURE", GameType.ADVENTURE);
         event.add("SPECTATOR", GameType.SPECTATOR);
+    }
+
+    @Override
+    public void loadCommonProperties(CommonProperties properties) {
+        boolean generate = properties.get("AdvJSExample", true);
+        DEBUG = properties.get("AdvJSDebug", true);
+        example(generate);
+    }
+
+    private static void example(boolean generate) {
+        if (generate && Files.notExists(AdvJS.EXAMPLE)) {
+            try {
+                Files.writeString(AdvJS.EXAMPLE, """
+                    // You Can Turn Off Auto Generate This Example In common.properties
+                    ServerEvents.advancement((event) => {
+                        const { BOUNDS, PREDICATE, TRIGGER } = event;
+
+                        // Define triggers
+                        const jump5times = TRIGGER.tick((triggerBuilder) =>
+                            triggerBuilder.addStat(Stats.JUMP, Stats.CUSTOM, BOUNDS.min$Integer(5)));
+                        const bred_in_nether = TRIGGER.bredAnimals((triggerBuilder) => {
+                            triggerBuilder.setChild(PREDICATE.entity({
+                                stepping_on: {
+                                    dimension: "the_nether"
+                                }
+                            }))
+                        });
+
+                        // Create root advancement
+                        const root = event.create("advjs:hell")
+                            .display((displayBuilder) => {
+                                displayBuilder.setTitle("AdvancementJS")
+                                displayBuilder.setDescription("Quick example")
+                                displayBuilder.setIcon("diamond")
+                            })
+                            .criteria((criteriaBuilder) => criteriaBuilder.add("tick", TRIGGER.tick()));
+
+                        // Add child for root
+                        root.addChild("child1", (childBuilder) => {
+                            childBuilder
+                                .display((displayBuilder) => {
+                                    displayBuilder.setTitle(Text.red("Holy"))
+                                    displayBuilder.setDescription(Text.red("Hell starts"))
+                                })
+                                .criteria((criteriaBuilder) => {
+                                    // 'OR' means that if you want to achieve this advancement,
+                                    // you just need one of two triggers matched below
+                                    criteriaBuilder.setStrategy(RequirementsStrategy.OR)
+                                    criteriaBuilder.add("bred", bred_in_nether)
+                                    criteriaBuilder.add("jump", jump5times)
+                                })
+                                .rewards((rewardsBuilder) => {
+                                    rewardsBuilder.setRecipes("minecraft:lodestone", "minecraft:brewing_stand")
+                                    rewardsBuilder.setExperience(100)
+                                })
+                        });
+
+                        // Remove an exist advancement
+                        event.remove("minecraft:story/lava_bucket")
+
+                        // modify an exist advancement
+                        event.get("minecraft:story/smelt_iron")
+                            .modifyDisplay((displayBuilder) => displayBuilder.setIcon("diamond_pickaxe"))
+                            .addChild("child2", (childBuilder) => {
+                                childBuilder
+                                    .display((displayBuilder) => {
+                                        displayBuilder.setTitle('A nice one!')
+                                        displayBuilder.setDescription(Text.green("Good luck"))
+                                    })
+                                    .criteria((criteriaBuilder) => criteriaBuilder.add("jump", jump5times))
+                            });
+                    })
+                    """
+                );
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            AdvJS.LOGGER.info("Generated advancement.js");
+        }
     }
 }
