abaf99da62ec8968902b68d78ea450a543e3cdab
Westernat <westernat233@163.com>
2024-01-08 18:29:28 +0800

    updates to latest

--- src/main/java/org/mesdag/advjs/trigger/custom/BossEventConditions.java
+++ src/main/java/org/mesdag/advjs/trigger/custom/BossEventConditions.java
@@ -5,7 +5,6 @@ import dev.latvian.mods.kubejs.typings.Info;
 import net.minecraft.advancement.criterion.AbstractCriterion;
 import net.minecraft.advancement.criterion.AbstractCriterionConditions;
 import net.minecraft.predicate.entity.AdvancementEntityPredicateDeserializer;
-import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
@@ -22,11 +21,12 @@ public class BossEventConditions extends AbstractCriterion<BossEventConditions.C
         boolean darkenScreen = jsonObject.has("darken_screen") && jsonObject.get("darken_screen").getAsBoolean();
         boolean playBossMusic = jsonObject.has("play_boss_music") && jsonObject.get("play_boss_music").getAsBoolean();
         boolean createWorldFog = jsonObject.has("create_world_fog") && jsonObject.get("create_world_fog").getAsBoolean();
-        return new Conditions(composite, darkenScreen, playBossMusic, createWorldFog);
+        String key = jsonObject.has("key") ? jsonObject.get("key").getAsString() : "";
+        return new Conditions(composite, darkenScreen, playBossMusic, createWorldFog, key);
     }
 
-    public void trigger(ServerPlayerEntity serverPlayer, boolean darkenScreen, boolean playBossMusic, boolean createWorldFog) {
-        this.trigger(serverPlayer, instance -> instance.matches(darkenScreen, playBossMusic, createWorldFog));
+    public void trigger(ServerPlayerEntity serverPlayer, boolean darkenScreen, boolean playBossMusic, boolean createWorldFog, String key) {
+        this.trigger(serverPlayer, instance -> instance.matches(darkenScreen, playBossMusic, createWorldFog, key));
     }
 
     @Override
@@ -37,13 +37,14 @@ public class BossEventConditions extends AbstractCriterion<BossEventConditions.C
     public static Conditions bossEvent(Consumer<Builder> consumer) {
         Builder builder = new Builder();
         consumer.accept(builder);
-        return new Conditions(builder.player, builder.darkenScreen, builder.playBossMusic, builder.createWorldFog);
+        return new Conditions(builder.player, builder.darkenScreen, builder.playBossMusic, builder.createWorldFog, builder.key);
     }
 
     public static class Builder extends AbstractTriggerBuilder {
         boolean darkenScreen = false;
         boolean playBossMusic = false;
         boolean createWorldFog = false;
+        String key = "";
 
         @Info("Check darken screen.")
         public void setDarkenScreen(boolean darkenScreen) {
@@ -59,22 +60,37 @@ public class BossEventConditions extends AbstractCriterion<BossEventConditions.C
         public void setCreateWorldFog(boolean createWorldFog) {
             this.createWorldFog = createWorldFog;
         }
+
+        @Info("""
+            Match 'translation key' or 'literal text' of boss bar.
+                        
+            If set to "", it will not check key. Defaults to "".
+            """)
+        public void setKey(String key) {
+            this.key = key;
+        }
     }
 
     public static class Conditions extends AbstractCriterionConditions {
         private final boolean darkenScreen;
         private final boolean playBossMusic;
         private final boolean createWorldFog;
+        private final String key;
 
-        public Conditions(LootContextPredicate composite, boolean darkenScreen, boolean playBossMusic, boolean createWorldFog) {
+        public Conditions(LootContextPredicate composite, boolean darkenScreen, boolean playBossMusic, boolean createWorldFog, String key) {
             super(ID, composite);
             this.darkenScreen = darkenScreen;
             this.playBossMusic = playBossMusic;
             this.createWorldFog = createWorldFog;
+            this.key = key;
         }
 
-        public boolean matches(boolean darkenScreen, boolean playBossMusic, boolean createWorldFog) {
-            return this.darkenScreen == darkenScreen && this.playBossMusic && playBossMusic && this.createWorldFog == createWorldFog;
+        public boolean matches(boolean darkenScreen, boolean playBossMusic, boolean createWorldFog, String key) {
+            if (!this.key.equals("") && !this.key.equals(key)) {
+                return false;
+            } else {
+                return this.darkenScreen == darkenScreen && this.playBossMusic && playBossMusic && this.createWorldFog == createWorldFog;
+            }
         }
     }
 }
