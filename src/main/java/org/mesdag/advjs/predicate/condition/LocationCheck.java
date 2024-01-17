c4c47795c057aaecda7ade3cc9287aec6187055e
Westernat <westernat233@163.com>
2024-01-17 13:11:14 +0800

    New Info, new child name condition

--- src/main/java/org/mesdag/advjs/predicate/condition/LocationCheck.java
+++ src/main/java/org/mesdag/advjs/predicate/condition/LocationCheck.java
@@ -1,5 +1,6 @@
 package org.mesdag.advjs.predicate.condition;
 
+import dev.latvian.mods.kubejs.typings.Info;
 import dev.latvian.mods.rhino.util.HideFromJS;
 import net.minecraft.loot.condition.LocationCheckLootCondition;
 import net.minecraft.loot.condition.LootCondition;
@@ -12,11 +13,13 @@ public class LocationCheck implements Check {
     LocationPredicateBuilder locationBuilder = new LocationPredicateBuilder();
     BlockPos offset = BlockPos.ORIGIN;
 
+    @Info("The location predicate of this check.")
     public LocationCheck location(Consumer<LocationPredicateBuilder> consumer) {
         consumer.accept(this.locationBuilder);
         return this;
     }
 
+    @Info("The offset block pos of this check.")
     public LocationCheck offset(BlockPos offset) {
         this.offset = offset;
         return this;
